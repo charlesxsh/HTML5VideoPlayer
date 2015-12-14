b16616db58f941647de19c2d03385ad04e8aef8c
Wang Yudi <ywang8@wpi.edu>
2015-12-13 23:59:08 -0500

    add bullet

--- public/js/handle-bullet.js
+++ public/js/handle-bullet.js
@@ -5,19 +5,35 @@ function handleBullet(socket){
 		cnt++;
 		var comment = msg.comment;
 		var time = msg.time;
-	$('.bulletdiv').css({
-		"width": "100px",
-	    "height": "50px",
-	    "font-weight": "bold",
-	    "position": "absolute",
-	    "z-index":"2147483647"
-	    // "animation": "mymove 5s"
-	});
-	$('section').append(
-		"<div id='bullet'" + cnt + " class='bulletdiv'></div>");
-	$(".bulletdiv").show("slide",{direction:"left"}, 1000);
+	// $('.bulletdiv').css({
+	// 	"width": "100px",
+	//     "height": "50px",
+	//     "font-weight": "bold",
+	//     "position": "absolute",
+	//     "z-index":"2147483647",
+	//     "top":"0"
+	//     // "animation": "mymove 5s"
+	// });
+	var bullet = document.createElement("div");
+	var parent = document.getElementById("playerarea");
+	bullet.id = "bullet"+cnt;
+	bullet.className += "bulletdiv";
+	bullet.innerHTML = comment;
+	parent.appendChild(bullet);
 
-	});
+    setTimeout(function() {
+        // var div2 = document.querySelector('#div2');
+        
+        parent.removeChild(bullet);
+    }, 6000);
+  
+
+
+	// $('section').append(
+	// 	"<div id='bullet'" + cnt + " class='bulletdiv'></div>");
+	// $(".bulletdiv").show("slide",{direction:"left"}, 1000);
+
+	// });
 
 
 	// @keyframes mymove {
@@ -28,4 +44,6 @@ function handleBullet(socket){
     //     var div = document.querySelector('.bulletdiv');
     // 	div.hidden = true;
     // }, 5000);
+});
+
 }
